c1fa1592d65f931d7fecd549ccf1344f35f3d4f4
Alejandro Ortiz <alejandro.ortiz@unosquare.com>
2018-11-13 16:22:28 -0600

    second

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import{Hero} from './hero'
+
+export const HEROES : Hero[] = [
+    {id: 11, name: 'Mr. Nice'},
+    {id: 12, name: 'Narco'},
+    {id: 13, name: 'Bombasto'},
+    {id: 14, name: 'Celeritas'},
+    {id: 15, name: 'Magenta'},
+    {id: 16, name: 'Rubberman'},
+    {id: 17, name: 'Dynama'},
+    {id: 18, name: 'Dr. IQ'},
+    {id: 19, name: 'Midman'},
+    {id: 20, name: 'Tornado'},
+
+] ;
+
